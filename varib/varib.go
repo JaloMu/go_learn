6193f8a98f5e025b207dd92181d2b1376af9f87f
JaloMu <jalo_mu@sina.com>
2017-08-27 03:20:07 +0800

    map 对象方法测试

--- varib/varib.go
+++ varib/varib.go
@@ -8,6 +8,7 @@ import (
 
 type slice []int
 type sli []*int
+type HEAD map[string][]string
 
 // 定义一个通用的类型切片
 type sl []interface{}
@@ -26,10 +27,15 @@ func (s *sl) Apend(i interface{}) {
 	*s = append(*s, i)
 }
 
+func (h HEAD) Add(key, value string) {
+	h[key] = append(h[key], value)
+}
+
 func main() {
 	var w, r int32 = 2, 2
 	fmt.Println(w, r, unsafe.Sizeof(w), unsafe.Sizeof(r))
-	var i slice = make([]int, 1)
+	var i slice
+	fmt.Println(i)
 	var ko []int
 	fmt.Println(len(ko), unsafe.Sizeof(ko))
 	fmt.Println("---------------")
@@ -44,11 +50,13 @@ func main() {
 	f := []interface{}{}
 	var n []int
 	m := []int{}
+	//////////////
+	fmt.Printf("%p---------------i: %v\n", i, i)
 	i.Apend(a)
-	fmt.Println(i)
+	fmt.Printf("%p---------------i: %v\n", i, i)
 	i.Aspend(a)
+	fmt.Printf("%p---------------i: %v\n", i, i)
 	p.Aspend(&a)
-	fmt.Println(i)
 	fmt.Println(p)
 
 	// 输出d的值
@@ -69,10 +77,35 @@ func main() {
 	fmt.Println(d)
 	d.Apend(c)
 	fmt.Println(d)
+
 	for _, v := range d {
 		is_type(v)
 		fmt.Println("v 占用空间大小：", unsafe.Sizeof(v))
 	}
+	var h HEAD = make(map[string][]string, 1)
+	fmt.Printf("%p, %v\n", h, h)
+	h.Add("1", "A")
+	h.Add("2", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	h.Add("3", "A")
+	fmt.Printf("%p, %v\n", h, h)
 }
 
 func is_type(value interface{}) {
